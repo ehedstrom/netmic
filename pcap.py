0e3828f4b19e150c28357fe7446ff50e69bc9321
Eric <eric@erichedstrom.com>
2022-08-31 11:50:05 -0700

    Fleshed out comments for pcap header.

--- pcap.py
+++ pcap.py
@@ -1,11 +1,20 @@
 from struct import pack
 import time
 
-# Builds and manages pcap.
+
+# Builds and manages libpcap file.
+# wiki.wireshark.org/Development/LibcapFileFormat
 class PCAPFile:
     def __init__(self, filename):
+        # Requires WB flag.
         self.fp = open(filename, "wb")
+
+        # Create PCAP header
+        # magic number, major version, minor version, GMT to Local,
+        # accuracy, max length of packets in octects, data link type
         header = pack('!IHHiIII', 0xa1b2c3d4, 2, 4, 0, 0, 65535, 1)
+
+        # Write the header.
         self.fp.write(header)
 
     def write(self, data):
